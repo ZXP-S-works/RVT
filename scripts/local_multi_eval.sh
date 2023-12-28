40080a76e69ba7da555a3f080d6dda7690df6b0b
ZXP-S-Works <zxpzzz521@gmail.com>
2023-12-28 11:28:08 -0500

    finishing 10demo training

--- scripts/local_multi_eval.sh
+++ scripts/local_multi_eval.sh
@@ -2,24 +2,26 @@ run()
 {
 cd ../rvt
 python eval.py \
-       --model-folder=runs/${date}/'rvt_tasks_'${task_name} \
+       --model-folder=runs/${date}/${n_demo}/${task_name} \
        --eval-datafolder=/home/zxp/projects/C2F_bi_equi/c2f_bi_equi_data/val \
        --tasks=${task_name} \
-       --eval-episodes=${eval_episodes} \
-       --log-name=${eval_episodes} \
+       --eval-episodes=100 \
+       --log-name=${n_demo}'demo' \
        --device=1 \
        --headless \
        --model-name=model_last.pth
 }
 
-#task_names=('place_cups' 'stack_cups' 'turn_tap' 'place_wine_at_rack_location')
-task_names=('place_cups' 'stack_cups')
+#task_names=('place_cups' 'light_bulb_in' 'place_wine_at_rack_location' 'put_groceries_in_cupboard'
+#'slide_block_to_color_target' 'sweep_to_dustpan_of_size' 'stack_blocks' 'close_jar' 'insert_onto_square_peg'
+#'put_money_in_safe' 'meat_off_grill' 'open_drawer' 'reach_and_drag' 'push_buttons' 'stack_cups' 'turn_tap'
+#'put_item_in_drawer' 'place_shape_in_shape_sorter')
 
-date=1222
-for eval_episodes in 2
+task_names=('place_cups')
+
+date=1226
+n_demo=10
+for task_name in "${task_names[@]}"
   do
-  for task_name in "${task_names[@]}"
-    do
-      run
-      done
+    run
     done
--- scripts/local_multi_train.sh
+++ scripts/local_multi_train.sh
@@ -9,12 +9,12 @@ python train.py \
       --n_demo=${n_demo}
 }
 
-task_names=('place_cups' 'light_bulb_in' 'place_wine_at_rack_location' 'put_groceries_in_cupboard'
-'slide_block_to_color_target' 'sweep_to_dustpan_of_size' 'stack_blocks' 'close_jar' 'insert_onto_square_peg'
-'put_money_in_safe' 'meat_off_grill' 'open_drawer' 'reach_and_drag' 'push_buttons' 'stack_cups' 'turn_tap'
-'put_item_in_drawer' 'place_shape_in_shape_sorter')
+#task_names=('place_cups' 'light_bulb_in' 'place_wine_at_rack_location' 'put_groceries_in_cupboard'
+#'slide_block_to_color_target' 'sweep_to_dustpan_of_size' 'stack_blocks' 'close_jar' 'insert_onto_square_peg'
+#'put_money_in_safe' 'meat_off_grill' 'open_drawer' 'reach_and_drag' 'push_buttons' 'stack_cups' 'turn_tap'
+#'put_item_in_drawer' 'place_shape_in_shape_sorter')
 
-#task_names=('place_cups')
+task_names=('place_shape_in_shape_sorter')
 
 date=1226
 for n_demo in 10
